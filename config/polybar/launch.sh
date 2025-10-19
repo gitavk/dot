cba346e81698bb8ab7d23b0903d2f5ebeb309ed4
gitavk <3apa3ka3@gmail.com>
2025-10-19 09:34:20 +0200

    Added alacritty zellij.

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -10,4 +10,8 @@ echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
+my_laptop_external_monitor=$(xrandr --query | grep 'DP2')
+if [[ $my_laptop_external_monitor = *connected* ]]; then
+  polybar bar2 2>&1 | tee -a /tmp/polybar1.log & disown
+fi
 echo "Bars launched..."
